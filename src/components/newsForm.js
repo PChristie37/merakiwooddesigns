9fda7ebf2f6cec247a5a70e15d661990c813c381
Peter <pete.hypho@hotmail.com>
2019-08-09 20:31:42 -0700

    testing a form submission

--- src/components/newsForm.js
+++ src/components/newsForm.js
@@ -21,6 +21,7 @@ function NewsForm() {
             </Button>
             {!formActive ? null : 
                 <form name="contact" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="contact" />
                     <p>
                         <label>Your Name: <input type="text" name="name" /></label>   
                     </p>
